bafd979c58a7aa8d4d761e52cba784efd7796adc
Eduin Frank Garcia Cordero <eduinlight@gmail.com>
2023-10-13 00:22:21 +0200

    Add readme

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -3,4 +3,5 @@
 Set-Alias -Name cat -Value bat.exe -Scope "Global" -Option "AllScope"
 Set-Alias -Name lsa -Value "lsd.exe -al" -Scope "Global" -Option "AllScope"
 
+#THEME
 oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\amro.omp.json" | Invoke-Expression

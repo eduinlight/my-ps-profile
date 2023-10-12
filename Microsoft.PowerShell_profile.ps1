b117516c563d06447576a7459e401fe6cb36d16b
Eduin Frank Garcia Cordero <eduinlight@gmail.com>
2023-10-13 00:19:07 +0200

    First Commit

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,6 @@
+
+#ALIASES
+Set-Alias -Name cat -Value bat.exe -Scope "Global" -Option "AllScope"
+Set-Alias -Name lsa -Value "lsd.exe -al" -Scope "Global" -Option "AllScope"
+
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\amro.omp.json" | Invoke-Expression
